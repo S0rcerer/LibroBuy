be9584844b0a8e9a3d8d12115cbfb5eb4cb875c2
Sorcerer <sashokbad@gmail.com>
2012-12-23 00:08:49 +0400

    Статистика пользователя

--- UserStatistic/UserStatistic/UserStatistic.cs
+++ UserStatistic/UserStatistic/UserStatistic.cs
@@ -12,10 +12,57 @@ namespace UserStatistic
 {
     public partial class UserStatistic : Form
     {
+        User statisticOwner;
+
         public UserStatistic(User user)
         {
             InitializeComponent();
             this.Text = user.login;
+
+            statisticOwner = user;
+
+            OutputUserProfile();
+            OutputHistory();
+
+
         }
+
+        private void OutputUserProfile()
+        {
+            label1.Text += statisticOwner.login;
+            label2.Text += statisticOwner.address;
+            label3.Text += statisticOwner.firstName + " " + statisticOwner.lastName;
+        }
+
+        private void OutputHistory()
+        {
+            decimal price = 0;
+            int totalBooks = 0;
+
+            foreach (Book[] books in statisticOwner.history)
+            {
+                foreach (Book book in books)
+                {
+                    String purchase = book.time.ToShortDateString() + " куплена книга " + book.title + ": " + book.copies + "*" + book.price + "$";
+                    listBox1.Items.Add(purchase);
+                    price += book.copies*book.price;
+                    totalBooks += book.copies;
+                }
+            }
+
+            float aveBooksAtOnce = totalBooks / statisticOwner.history.Length;
+            float aveCostAtOnce = (float)price / statisticOwner.history.Length;
+
+            label4.Text += aveBooksAtOnce.ToString();
+            label5.Text += aveCostAtOnce.ToString() + "$";
+
+            label6.Text += totalBooks;
+            label7.Text += price + "$";
+
+        }
+
+
+
+        
     }
 }
