0c1197c5a6d21c4efbf9c7c107b5107296f7c5fc
Sorcerer <sashokbad@gmail.com>
2012-12-22 22:36:08 +0400

    Первый коммит

---
+++ UserStatistic/UserStatistic/UserStatistic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UserStatistic
+{
+    public partial class UserStatistic : Form
+    {
+        public UserStatistic()
+        {
+            InitializeComponent();
+        }
+    }
+}
